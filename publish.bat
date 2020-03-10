e2f49bdb40efc69c2866f268a072059d8524acac
suremaker <wojciech.kotlarski@gmail.com>
2020-03-10 20:29:54 +0000

    Updated scripts and config

--- publish.bat
+++ publish.bat
@@ -1,4 +1,5 @@
 rd /S /Q out
 mkdir out
-xcopy /E /Y bin out
+xcopy /E /Y template out
+copy /Y JiraEpicRoadmapper\Server\config.json out\config.json
 dotnet publish JiraEpicRoadmapper\Server\JiraEpicRoadmapper.Server.csproj -o out\server
---
+++ template/JiraEpicRoadmapper.bat
@@ -0,0 +1,3 @@
+copy /Y config.json server/.
+start server/JiraEpicRoadmapper.Server.exe
+start http://localhost:5000/
