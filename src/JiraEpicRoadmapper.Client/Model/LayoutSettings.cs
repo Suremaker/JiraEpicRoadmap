6ee790b1b0f4ed3a0da57480c64277f1b1fe19e7
suremaker <wojciech.kotlarski@gmail.com>
2020-03-14 18:19:07 +0000

    adjusted size

--- src/JiraEpicRoadmapper.Client/Model/LayoutSettings.cs
+++ src/JiraEpicRoadmapper.Client/Model/LayoutSettings.cs
@@ -2,10 +2,10 @@
 {
     public static class LayoutSettings
     {
-        public const int MinBlockLength = 3;
-        public static readonly int DaySpan = 60;
+        public const int MinBlockLength = 4;
+        public static readonly int DaySpan = 70;
         public static readonly int CellMargin = 10;
-        public static readonly int TicketHeight = 50;
+        public static readonly int TicketHeight = 60;
         public static readonly int RowMargin = 10;
         public static readonly int RowHeight = TicketHeight + 2 * RowMargin;
     }
