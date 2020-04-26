2a33d25d6a6eb8c49abe5c4af36c4bf065d35fa4
suremaker <wojciech.kotlarski@gmail.com>
2020-04-26 10:37:28 +0100

    Added Api skeleton

--- publish.bat
+++
@@ -1,1 +0,0 @@
-dotnet publish JiraEpicVisualizer\JiraEpicVisualizer.csproj -r win-x64 -c Release --self-contained=true /p:PublishTrimmed=true /p:PublishSingleFile=true -o out
---
+++ src/JiraEpicRoadmapper.Server/Controllers/EpicsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JiraEpicRoadmapper.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EpicsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            yield return "ok";
+        }
+    }
+}
