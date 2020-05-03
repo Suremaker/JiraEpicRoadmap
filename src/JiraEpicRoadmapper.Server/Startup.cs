1257ff1b586120bfb156e281409861977edacc19
suremaker <wojciech.kotlarski@gmail.com>
2020-05-03 14:28:40 +0100

    Added swagger

--- src/JiraEpicRoadmapper.Server/Startup.cs
+++ src/JiraEpicRoadmapper.Server/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace JiraEpicRoadmapper.Server
 {
@@ -19,6 +20,10 @@ namespace JiraEpicRoadmapper.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "JiraEpicRoadmapper API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +36,11 @@ namespace JiraEpicRoadmapper.Server
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JiraEpicRoadmapper API V1");
+            });
             app.UseRouting();
 
             app.UseAuthorization();
@@ -39,6 +49,8 @@ namespace JiraEpicRoadmapper.Server
             {
                 endpoints.MapControllers();
             });
+
+
         }
     }
 }
